e2742d22e57e143f8bb5ddabd93bb24b90a3ec51
Unknown <depifan3@msu.edu>
2018-01-29 00:06:33 -0500

    Update

--- scripts/button_change.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 380) {
-    $(".menu").removeClass("white").addClass("black");
-  } else {
-    $(".menu").removeClass("black").addClass("white");
-  }
-});
